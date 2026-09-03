78cf0d79c8ab06ecd0d194272498e2e60c961e21
Zaid Asaireh <zadesamer09@gmail.com>
2026-09-03 18:55:44 +0300

    feat(frontend): deliver the marketplace experience

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,10 @@
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
+
+function Toaster(props: ToasterProps) {
+  const { theme = "system" } = useTheme();
+
+  return <Sonner theme={theme as ToasterProps["theme"]} {...props} />;
+}
+
+export { Toaster };
